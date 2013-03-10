b384c663fb5e7ee9511dfcb63fbbfce506f08fe3
John Tantalo <john.tantalo@gmail.com>
2013-03-09 16:20:43 -0800

    added yeast, salt, sugar, oil

--- breadr.js
+++ breadr.js
@@ -18,9 +18,21 @@ Breadr.LIQUID_GRAMS_PER_LOAF = 265;
 
 Breadr.FLOUR_PER_LIQUID_MASS = 1.5;
 
+Breadr.YEAST_PER_FLOUR_MASS = .025;
+
+Breadr.SALT_PER_FLOUR_MASS = .019;
+
+Breadr.SUGAR_PER_FLOUR_MASS = .047;
+
+Breadr.OIL_PER_FLOUR_MASS = .034;
+
 Breadr.prototype.generateRecipe = function () {
   var liquid = this.loaves * Breadr.LIQUID_GRAMS_PER_LOAF;
   var flour = liquid * Breadr.FLOUR_PER_LIQUID_MASS;
+  var yeast = flour * Breadr.YEAST_PER_FLOUR_MASS;
+  var salt = flour * Breadr.SALT_PER_FLOUR_MASS;
+  var sugar = flour * Breadr.SUGAR_PER_FLOUR_MASS;
+  var oil = flour * Breadr.OIL_PER_FLOUR_MASS;
   return [{
       name: 'liquid',
       amount: Math.floor(liquid),
@@ -28,6 +40,26 @@ Breadr.prototype.generateRecipe = function () {
     {
       name: 'flour',
       amount: Math.floor(flour),
+      unit: 'g'
+    },
+    {
+      name: 'yeast',
+      amount: Math.floor(yeast),
+      unit: 'g'
+    },
+    {
+      name: 'salt',
+      amount: Math.floor(salt),
+      unit: 'g'
+    },
+    {
+      name: 'sugar',
+      amount: Math.floor(sugar),
+      unit: 'g'
+    },
+    {
+      name: 'oil',
+      amount: Math.floor(oil),
       unit: 'g'
     }
   ];
